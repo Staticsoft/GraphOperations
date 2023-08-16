2fdcbe8a390399daa57770e556e251cf02511928
Oleg Rakovitch <devgeslot@gmail.com>
2023-08-16 18:30:58 -0400

    Detailed error messages

---
+++ GraphOperations.Abstractions/GraphProcessor.cs
@@ -0,0 +1,15 @@
+﻿namespace Staticsoft.GraphOperations.Abstractions;
+
+public interface GraphProcessor
+{
+    Task<object> Process(string serializedGraph);
+}
+
+public class GraphProcessorException : Exception
+{
+    public GraphProcessorException(string message)
+        : base(message) { }
+
+    public GraphProcessorException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
