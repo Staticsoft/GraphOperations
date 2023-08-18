db2de9aa32da0c22e75080dde0ff6d10da2a81ef
OlegRakovitch <devgeslot@gmail.com>
2023-08-18 12:58:46 -0400

    Improved deserialization error messages

--- GraphOperations.Memory/ObjectSerializer.cs
+++ GraphOperations.Memory/ObjectSerializer.cs
@@ -14,6 +14,14 @@ public class ObjectSerializer
     }
 
     public object ToType(object obj, Type targetType)
-        => JsonSerializer.Deserialize(JsonSerializer.Serialize(obj), targetType, Options)
-        ?? throw new InvalidCastException($"Cannot convert object of type '{obj.GetType().Name}' to type '{targetType.Name}'");
+        => ToType(JsonSerializer.Serialize(obj), targetType);
+
+    object ToType(string serialized, Type targetType)
+        => Try
+            .Return(() => JsonSerializer.Deserialize(serialized, targetType, Options))
+            .On<JsonException>((ex) => DeserializationError(serialized, targetType, ex))
+            .Result() ?? DeserializationError(serialized, targetType, new ArgumentNullException());
+
+    static InvalidCastException DeserializationError(string serialized, Type targetType, Exception inner)
+        => new($"Unable to deserialize object `{serialized}` to type {targetType.Name}", inner);
 }
